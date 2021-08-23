2bf7510270dcab46d85b45298f0bec5aed363506
Dom J <dom1jaski@gmail.com>
2021-08-23 11:22:32 +1000

    Delete ROI ALIGN 3D IDea.R

--- ROI ALIGN 3D IDea.R
+++
@@ -1,8 +0,0 @@
-DOM DOM DOM !!! THIS IS WHERE I HAVE TO 
-
-from cuda_functions.roi_align_3D.roi_align.crop_and_resize import CropAndResizeFunction as ra3D
-
-https://github.com/MIC-DKFZ/medicaldetectiontoolkit/blob/master/cuda_functions/roi_align_3D/roi_align/crop_and_resize.py
-
-from ._ext import crop_and_resize as _backend
-
